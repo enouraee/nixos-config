e9782652c8d08fc13afd1c42d117e1363b232d8f
Eilia <eilia@Mac.tomanpay.local>
2026-02-02 19:04:11 +0330

    fix: change noto-fonts-emoji to noto-fonts-color-emoji

--- modules/common/fonts.nix
+++ modules/common/fonts.nix
@@ -6,7 +6,7 @@
     # System fonts
     noto-fonts
     noto-fonts-cjk-sans
-    noto-fonts-emoji
+    noto-fonts-color-emoji
 
     # Programming fonts with Nerd Font icons
     (nerdfonts.override { fonts = [ "JetBrainsMono" "FiraCode" ]; })
